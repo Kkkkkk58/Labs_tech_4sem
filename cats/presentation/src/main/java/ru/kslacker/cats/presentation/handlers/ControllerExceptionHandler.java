9213708a4f73d8e475768da2183705dfa8896dec
Karim Khasan <karimhasan3879@gmail.com>
2023-04-15 09:00:11 +0300

    chore: remove unused classes

--- cats/presentation/src/main/java/ru/kslacker/cats/presentation/handlers/ControllerExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.kslacker.cats.presentation.handlers;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ControllerExceptionHandler {
-
-}
